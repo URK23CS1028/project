449111735f097cc830ad4dcdfef0a45c5f700a5f
Hisham Raihan <hishamraihan@karunya.edu.in>
2024-01-09 11:58:45 +0530

    This program is used to show exception handling

---
+++ sum_numbers.py
@@ -0,0 +1,22 @@
+
+#explanation how to raise exeption errors 
+#with try expect 
+
+
+class numbers:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def sum(self):
+        try:
+            return self.a+self.b
+        except Exception as e:
+            #print(e)
+            return 'The both numbers should be of int float or double'
+        #finally:
+            return "the error is raised"
+
+
+print("ww")
+object32=numbers(int(input()),input())
+print(object32.sum())

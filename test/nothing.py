28c1271244708eb1448b680b2f13a8b6bbfa27bf
URK23CS1028 <153587950+URK23CS1028@users.noreply.github.com>
2024-01-23 11:27:07 +0530

    this program is to show the addition of two number using class handling

--- test/nothing.py
+++ test/nothing.py
@@ -5,7 +5,7 @@ class add2:
   def addition(self):
     return self.a+self.b
 
-__nane__='__main__':
+__name__='__main__':
 number_1=int(input("Enter the first number:"))
 number_2=int(input("Enter the second number:))
 object32=add2(number_1,number_2)
